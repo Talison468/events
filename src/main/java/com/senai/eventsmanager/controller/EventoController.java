c2c401980ba71fdc578aaeabbb9c858e42b58595
talison_b_cardoso <talisonbcardoso30@gmail.com>
2025-10-01 17:16:27 -0300

    teste

---
+++ src/main/java/com/senai/eventsmanager/controller/EventoController.java
@@ -0,0 +1,50 @@
+package com.senai.eventsmanager.controller;
+
+import com.senai.eventsmanager.dto.EventoDTO;
+import com.senai.eventsmanager.service.EventoService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/evento")
+public class EventoController {
+    @Autowired
+    EventoService service;
+
+    // pegar um evento pelo seu id
+    @GetMapping("/{id}")
+    public EventoDTO findById(@PathVariable("id") Long id){
+        return service.findById(id);
+    }
+    // pegar todos os eventos
+    @GetMapping
+    public List<EventoDTO> findAll(){
+        return service.findAll();
+    }
+    // salvar um evento
+    @PostMapping
+    public EventoDTO save(
+            @RequestBody @Valid EventoDTO eventoCreateDTO ){
+        return service.save(eventoCreateDTO);
+    }
+    // atualizar um evento
+    @PutMapping("/{id}")
+    public EventoDTO update(
+            @PathVariable("id")Long id,
+            @RequestBody EventoDTO eventoCreateDTO){
+        return service.update(id,eventoCreateDTO);
+    }
+    // deletar um evento pelo seu id
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id")Long id){
+
+        service.deleteById(id);
+    }
+
+
+}
---
+++ src/main/java/com/senai/eventsmanager/service/UsuarioService.java
@@ -0,0 +1,57 @@
+package com.senai.eventsmanager.service;
+
+import com.senai.eventsmanager.dto.UsuarioDTO;
+import com.senai.eventsmanager.entity.Usuario;
+import com.senai.eventsmanager.repository.UsuarioRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository repository;
+
+    public UsuarioDTO findById(Long id) {
+        Usuario usuario = repository.findById(id).orElseThrow();
+        return toDto(usuario);
+    }
+    public UsuarioDTO save(UsuarioDTO usuarioDto) {
+        Usuario usuario = toEntity(usuarioDto);
+        usuario.setCreatedAt(LocalDateTime.now());
+        usuario.setUpdatedAt(LocalDateTime.now());
+        usuario = repository.save(usuario);
+        return toDto(usuario);
+     }
+    public UsuarioDTO update(Long id, UsuarioDTO usuarioDto) {
+        Usuario usuario = toEntity(usuarioDto);
+        usuario.setId(id);
+        usuario = repository.save(usuario);
+        return toDto(usuario);
+    }
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+    public List<UsuarioDTO> findAll() {
+        List<Usuario> usuarios = repository.findAll();
+        List<UsuarioDTO> usuariosDto = new ArrayList<>();
+        for (Usuario usuario : usuarios) {
+            usuariosDto.add(toDto(usuario));
+        }
+        return usuariosDto;
+    }
+    public UsuarioDTO toDto(Usuario usuario) {
+        UsuarioDTO DTO = new UsuarioDTO();
+        BeanUtils.copyProperties(usuario, DTO);
+        return DTO;
+    }
+
+    public Usuario toEntity(UsuarioDTO DTO){
+        Usuario usuario = new Usuario();
+        BeanUtils.copyProperties(DTO, usuario);
+        return usuario;
+    }
+}
